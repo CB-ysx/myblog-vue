5c1415a0f08d481e8eb5872ac0a9ec1fc7908050
joriscai <516003559@qq.com>
2017-03-18 18:55:26 +0800

    add a Foot component

---
+++ src/components/common/Foot.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer>
+    <p>
+      Copyright © 2017-{{dateNow}} <a href="#">joris' blog</a>
+    </p>
+    <p>
+      粤ICP备xxxxxx号
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'foot',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    dateNow: function () {
+      let date = new Date()
+      return date.getFullYear()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  .container{
+    max-width: 840px;
+    margin: 0 auto;
+  }
+  footer{
+    text-align: center;
+  }
+</style>
