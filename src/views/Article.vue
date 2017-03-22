9db1b19b33700d3f31051d6f96edd4989577a473
joriscai <516003559@qq.com>
2017-03-22 19:28:12 +0800

    finsh layout of the Article.vue, change the page's data template

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,21 +1,58 @@
 <template>
   <div class="article container content">
-    {{ title }}
+    <div class="main">
+      <article>
+        <h1 class="title">{{ article.title }}</h1>
+        <span class="date">{{ article.date }}</span>
+        <section class="text" v-html="markdownHtml(article.contents)"></section>
+      </article>
+    </div>
   </div>
 </template>
 
 <script>
+import { markdown } from '@/filters/markdown'
 export default {
   name: 'article',
   data () {
     return {
-      title: 'this is a Article page'
+      title: 'this is a Article page',
+      article: {
+        title: '',
+        date: '',
+        contents: ''
+      }
     }
+  },
+  methods: {
+    // parse the markdown language to HTML method
+    markdownHtml: function (str) {
+      return markdown(str)
+    }
+  },
+  mounted () {
+    // get to layout data
+    console.log(this.$route.params.id)
+    this.$http.get('/article/' + this.$route.params.id).then(res => {
+      console.log(res.url)
+      this.article = res.data
+    }, res => {
+      console.log(res)
+    })
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style scoped lang="scss">
+.title{
+  font-weight: 500;
+  font-size: 1.8em;
+}
+.date{
+  padding-bottom: 10px;
+}
+article{
+  padding: 0.5em 0.8em;
+}
 </style>
