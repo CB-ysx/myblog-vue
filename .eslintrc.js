ad8d9e5df4f31301e896dcb74b638ebdb9951657
joriscai <516003559@qq.com>
2017-03-26 16:33:40 +0800

    change the rule of 'operator-linebreak'

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   // add your custom rules here
   'rules': {
     'no-tabs': 'off',
+    'operator-linebreak': ["error", "before"],
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
