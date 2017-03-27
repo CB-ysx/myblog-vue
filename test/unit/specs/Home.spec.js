8d33d3e7a172d4274332cef6cfc88d45b2ee0c89
joriscai <516003559@qq.com>
2017-03-27 23:11:02 +0800

    add test case for TimeLine.vue

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Home from '@/views/Home'
 import Article from '@/views/Article'
+import TimeLine from '@/views/TimeLine'
 import HeadNav from '@/components/HeadNav'
 import Foot from '@/components/Foot'
 
@@ -30,6 +31,45 @@ describe('Article.vue', () => {
     vm.$el.querySelector('#pay').click()
     expect(vm.payShow)
       .to.false
+  })
+})
+
+// TimeLine.vue test case
+describe('TimeLine.vue', () => {
+  const Constructor = Vue.extend(TimeLine)
+  const vm = new Constructor().$mount()
+  it('should render correct contents', () => {
+    expect(vm.$el.classList.contains('time-line'))
+      .to.true
+  })
+  it('should render correct timeLines', () => {
+    vm.timeLines = [{'id': 1,
+      'title': 'test01',
+      'date': '2005-12-17'
+    },
+    {'id': 8,
+      'title': 'test08',
+      'date': '1998-04-16'
+    },
+    {'id': 5,
+      'title': 'test05',
+      'date': '2002-08-01'
+    }]
+    let test = [{'id': 1,
+      'title': 'test01',
+      'date': '2005-12-17'
+    },
+    {'id': 5,
+      'title': 'test05',
+      'date': '2002-08-01'
+    },
+    {'id': 8,
+      'title': 'test08',
+      'date': '1998-04-16'
+    }]
+    vm.sorted // trigger the sorted function
+    expect(vm.timeLines)
+      .to.deep.equal(test)
   })
 })
 
