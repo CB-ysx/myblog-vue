b23246eb9b30594b7485429d8d40aa805b5762b1
joriscai <516003559@qq.com>
2017-04-14 23:30:48 +0800

    add a markdown component is named Preview.vue

---
+++ src/components/Preview.vue
@@ -0,0 +1,31 @@
+/**
+ * Created by joriscai on 2017/4/14.
+ * This is Preview.vue
+ */
+<template>
+  <div class="preview-box">
+    <link href="//cdn.bootcss.com/github-markdown-css/2.4.1/github-markdown.css" rel="stylesheet">
+    <link href="//cdn.bootcss.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">
+    <section class="preview markdown-body" v-html="markdownHtml(contents)">
+    </section>
+  </div>
+</template>
+
+<script>
+import { markdown } from '@/filters/markdown'
+// import simplemde from 'simplemde'
+export default {
+  name: 'preview',
+  props: ['contents'],
+  methods: {
+    // parse the markdown language to HTML method
+    markdownHtml: function (str) {
+      return markdown(str)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+</style>
