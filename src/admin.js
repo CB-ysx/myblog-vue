58d3eb9098b815da4adadfa276b3c644d7830743
joriscai <516003559@qq.com>
2017-05-05 14:34:39 +0800

    register a focus global directive in admin.js

--- src/admin.js
+++ src/admin.js
@@ -10,6 +10,15 @@ Vue.use(VueResource)
 
 Vue.config.productionTip = false  // 关闭提示处于开发环境
 
+// 注册一个全局自定义指令 v-focus
+Vue.directive('focus', {
+  // 当绑定元素插入到 DOM 中。
+  inserted: function (el) {
+    // 聚焦元素
+    el.focus()
+  }
+})
+
 /* eslint-disable no-unused-vars */
 var vm = new Vue({
   el: '#app',
