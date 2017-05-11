15c0950f6dd58ffc9273961adb01c5a0f378a461
joriscai <516003559@qq.com>
2017-05-11 17:41:48 +0800

    fix the post method mock

--- src/models/Mock-private.js
+++ src/models/Mock-private.js
@@ -3,17 +3,17 @@ import Mock from 'mockjs'
 /**
  * Methods
  */
-Mock.mock(/\/private\/article\/\w{1,}/, /(post|put|delete)/, (ops) => {
-  return ops.type
+Mock.mock(/\/private\/article(\/\w{1,})?/, /(post|put|delete)/, (ops) => {
+  return ops
 })
-Mock.mock(/\/private\/timeline\/\w{1,}/, /(post|put|delete)/, (ops) => {
-  return ops.type
+Mock.mock(/\/private\/timeline(\/\w{1,})?/, /(post|put|delete)/, (ops) => {
+  return ops
 })
-Mock.mock(/\/private\/image\/\w{1,}/, /(post|put|delete)/, (ops) => {
-  return ops.type
+Mock.mock(/\/private\/image(\/\w{1,})?/, /(post|put|delete)/, (ops) => {
+  return ops
 })
-Mock.mock(/\/private\/about\/\w{1,}/, /(post|put|delete)/, (ops) => {
-  return ops.type
+Mock.mock(/\/private\/about(\/\w{1,})?/, /(post|put|delete)/, (ops) => {
+  return ops
 })
 
 export default Mock
