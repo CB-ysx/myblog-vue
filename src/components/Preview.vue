c9927e5067ee00feaa51d30d9fd4430437038d8d
joriscai <516003559@qq.com>
2017-06-08 22:06:14 +0800

    finsh the upload image layout when edit articles

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -20,7 +20,51 @@ export default {
   methods: {
     // parse the markdown language to HTML method
     markdownHtml: function (str) {
-      return markdown(str)
+      return markdown(str, true)
+    }
+  },
+  updated () {
+    let uploads = document.querySelectorAll('.upload-box')
+    if (uploads.length) {
+      for (let i = 0, len = uploads.length; i < len; i++) {
+        let input = uploads[i].querySelector('input')
+        // alert a file selector
+        uploads[i].querySelector('.paste').addEventListener('click', function (e) {
+          input.click()
+        }, true)
+        // upload the image
+        input.addEventListener('change', (e) => {
+          // let filename = input.files[0].name
+          // console.log(this.contents.split(/!\[(\w||\s)*\]\(\)/g))
+          let data = new FormData()
+          data.append('image', input.files[0])
+          this.$http.post('/private/image', data).then(res => {
+            console.log(res)
+            let img = new Image()
+            img.src = '123'
+            uploads[i].nextSibling.appendChild(img)
+            uploads[i].remove()
+          }, res => {
+            console.log(res)
+          })
+        })
+        // paste
+        uploads[i].addEventListener('paste', (e) => {
+          e.preventDefault()
+          console.log(e.clipboardData.items)
+          if (e.clipboardData && e.clipboardData.items[0].type.indexOf('image') > -1) {
+            // let reader = new FileReader()
+            let file = e.clipboardData.items[0].getAsFile()
+            let data = new FormData()
+            data.append('image', file)
+            this.$http.post('/url', data).then(res => {
+              console.log(res)
+            }, res => {
+              console.log(res)
+            })
+          }
+        }, false)
+      }
     }
   }
 }
@@ -28,4 +72,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
+.preview-box{
+  .upload-box{
+    position: relative;
+    cursor: pointer;
+    padding: 1em;
+    width: 100%;
+    background: #f6f7f8;
+    border-radius: 4px;
+    color: #808284;
+    text-align: center;
+  }
+  .paste{
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
--- src/filters/markdown.js
+++ src/filters/markdown.js
@@ -1,5 +1,24 @@
 import marked from 'marked'
 
+let renderer = new marked.Renderer()
+
+renderer.image = function (href, title, text) {
+  if (!href.trim().length) {
+    return `<div class="upload-box">\
+        <span>Click here upload</span>\
+        <input type="file" accept="image/*" class="hidden"/>\
+        <div class="paste" contenteditable readonly></div>\
+        </div>`
+  }
+
+  let out = '<img src="' + href + '" alt="' + text + '"'
+  if (title) {
+    out += ' title="' + title + '"'
+  }
+  out += '/>'
+  return out
+}
+
 marked.setOptions({
   gfm: true,
   pedantic: false,
@@ -10,6 +29,9 @@ marked.setOptions({
   smartypants: true
 })
 
-export function markdown (str) {
+export function markdown (str, editable) {
+  if (editable) {
+    return marked(str, { renderer: renderer })
+  }
   return marked(str)
 }
