a22469fc85dd6cc4a2d05853de9283a7f2db9c96
joriscai <516003559@qq.com>
2017-07-23 17:59:02 +0800

    imporve upload article image feature, optimize the code

--- src/components/PreviewEdit.vue
+++ src/components/PreviewEdit.vue
@@ -20,7 +20,8 @@ export default {
   props: ['contents'],
   data () {
     return {
-      context: this.contents
+      context: this.contents,
+      imgHost: ''
     }
   },
   watch: {
@@ -33,86 +34,113 @@ export default {
   },
   methods: {
     // parse the markdown language to HTML method
-    markdownHtml: function (str) {
+    markdownHtml (str) {
       return markdown(str, true)
     },
-    replaceByIndex: function (str, march, index, newStr) {
-      let temp = str.match(march)
-      let x = str.split(march)
+    // replace markdown Syntax of image by index
+    replaceByIndex (str, reg, index, newStr) {
       let num = -1
-      let i = 0
-      for (let len = x.length; i < len; i++) {
-        if (typeof x[i] === 'undefined') {
-          ++num
-          if (num === index) {
-            x[i] = newStr
-          } else {
-            x[i] = temp[num]
-          }
+      return str.replace(reg, function (match, offset, string) {
+        console.log('replace')
+        num++
+        if (num === index) {
+          return newStr
+        }
+        return match
+      })
+    },
+    // add url and title for markdown Syntax of image
+    replaceMdImg (index, filename, url) {
+      this.context = this.replaceByIndex(this.contents, /!\[.*\]\(\)/g, index, `![${filename}](${this.imgHost}/${url} "${filename}")`)
+    },
+    // image upload cancel handle
+    uploadCancel (cancel, ready, upload) {
+      console.log(cancel.handle)
+      typeof cancel.handle === 'function' && cancel.handle()
+      ready.removeAttribute('style')
+      upload.removeAttribute('style')
+    },
+    // generate the vue-resource options about progess...
+    uploadMethods (cancel, ready, upload, progress) {
+      return {
+        before (req) {
+          ready.style.display = 'none'
+          upload.style.display = 'block'
+          // get abort function
+          this.$nextTick(() => {
+            cancel.handle = req.abort
+          })
+        },
+        progress (e) {
+          let val = e.loaded / e.total * 100
+          console.log(val + '%')
+          progress.value = val
         }
       }
-      return x.join('')
     }
   },
   beforeCreate () {
     url = window.blogUrl.articleImg
+  },
+  mounted () {
+    // get upload image hostname and port
+    this.imgHost = url.match(/^(((\w+):)*\/\/)([^/:]+)(:\d*)?/g)
   },
   updated () {
     let uploads = document.querySelectorAll('.upload-box')
     if (uploads.length) {
       for (let i = 0, len = uploads.length; i < len; i++) {
         let input = uploads[i].querySelector('input')
+        let progress = uploads[i].querySelector('progress')
+        let cancelBtn = uploads[i].querySelector('button')
+        let pasteBox = uploads[i].querySelector('.paste')
+
+        let readyBox = uploads[i].querySelector('.ready')
+        let uploadingBox = uploads[i].querySelector('.uploading')
+        let cancel = {}
+        // cancel a file to upload
+        cancelBtn.addEventListener('click', () => {
+          this.uploadCancel(cancel, readyBox, uploadingBox)
+        }, true)
         // alert a file selector
-        uploads[i].querySelector('.paste').addEventListener('click', function (e) {
+        pasteBox.addEventListener('click', function (e) {
+          console.log(input)
           input.click()
         }, true)
         // upload the image
         input.addEventListener('change', (e) => {
-          // function replaceByIndex (str, march, index, newStr) {
-          //   let x = str.split(march)
-          //   let num = -1
-          //   let i = 0
-          //   for (len = x.length; i < len; i++) {
-          //     if (typeof x[i] === 'undefined') {
-          //       ++num
-          //       if (num === index) {
-          //         x[i] = newStr
-          //       } else {
-          //         x[i] = '![]()'
-          //       }
-          //     }
-          //   }
-          //   return x.join('')
-          // }
-
           let data = new FormData()
           let filename = input.files[0].name
           data.append('image', input.files[0])
-          this.$http.post(url, data).then(res => {
-            // let img = new Image()
-            // img.src = '123'
-            // uploads[i].nextSibling.appendChild(img)
-            // uploads[i].remove()
-            this.context = this.replaceByIndex(this.contents, /!\[(\w|\s)*\]\(\)/g, i, `![${filename}](${res.data.url})`)
+          this.$http.post(url, data, this.uploadMethods(cancel, readyBox, uploadingBox, progress)).then(res => {
+            console.log('uploaded')
+            // this.context = this.replaceByIndex(this.contents, /!\[.*\]\(\)/g, i, `![${filename}](${this.imgHost}/${res.data.url})`)
+            // replace origin text
+            this.replaceMdImg(i, filename, res.data.url)
           }, res => {
             console.log(res)
           })
         })
+
         // paste
-        uploads[i].addEventListener('paste', (e) => {
+        pasteBox.addEventListener('paste', (e) => {
           e.preventDefault()
-          console.log(e.clipboardData.items)
+          console.log(e)
+          console.log('paste')
           if (e.clipboardData && e.clipboardData.items[0].type.indexOf('image') > -1) {
-            // let reader = new FileReader()
             let file = e.clipboardData.items[0].getAsFile()
             let data = new FormData()
             data.append('image', file)
-            this.$http.post(url, data).then(res => {
+            this.$http.post(url, data, this.uploadMethods(cancel, readyBox, uploadingBox, progress)).then(res => {
               console.log(res)
-              this.context = this.replaceByIndex(this.contents, /!\[(\w|\s)*\]\(\)/g, i, `![](${res.url})`)
+              // replace origin text
+              this.replaceMdImg(i, '', res.data.url)
             }, res => {
               console.log(res)
             })
+          } else {
+            // warn alert
+            alert('Please copy a image to clipboard!')
           }
         }, false)
       }
@@ -141,6 +169,9 @@ export default {
     left: 0;
     width: 100%;
     height: 100%;
+  }
+  .progress{
+    width: 100%;
   }
 }
 </style>
--- src/filters/markdown.js
+++ src/filters/markdown.js
@@ -4,18 +4,25 @@ let renderer = new marked.Renderer()
 
 renderer.image = function (href, title, text) {
   if (!href.trim().length) {
-    return `<div class="upload-box">\
-        <span>Click here upload</span>\
-        <input type="file" accept="image/*" class="hidden"/>\
-        <div class="paste" contenteditable readonly></div>\
+    return `<div class="upload-box">
+          <div class="ready">
+            <span>Click here upload</span>
+            <input type="file" accept="image/*" class="hidden"/>
+            <div class="paste" contenteditable readonly></div>
+          </div>
+          <div class="uploading hidden">
+            <span>Uploading</span>
+            <progress value="0" max="100" class="progress"></progress>
+            <button class="">Cancel upload</button>
+          </div>
         </div>`
   }
 
-  let out = '<img src="' + href + '" alt="' + text + '"'
+  let out = `<img src="${href}" alt="${text}"`
   if (title) {
-    out += ' title="' + title + '"'
+    out += ` title="${title}"`
   }
-  out += '/>'
+  out += `/>`
   return out
 }
 
