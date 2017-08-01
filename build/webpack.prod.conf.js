3b41a7b2472f8134255bb13623cc392a83e29853
joriscai <516003559@qq.com>
2017-08-01 23:51:08 +0800

    fix not extract css to single file when load modules lazily

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -41,7 +41,8 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.assetsPath('css/[name].[contenthash].css'),
+      allChunks: true // fix not extract css to single file when load modules lazily
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
